09c6fd3dd0c579f885ab4a053b932d0e39753aca
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2021-01-17 04:02:31 +0900

    commit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'environment'
+const { environment } = require('@rails/webpacker')
 
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
