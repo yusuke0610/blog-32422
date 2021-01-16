fde69ded8084f459e261a27d291fe4fe4d7e418b
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2021-01-17 03:57:54 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-
-// require("../jquery-3.5.1")
-// require("../bootstrap")
 
 
 
@@ -21,4 +13,3 @@ require("jquery")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,5 @@
-const { environment } = require('@rails/webpacker')
+process.env.NODE_ENV = process.env.NODE_ENV || 'environment'
 
-const webpack = require('webpack')
+const environment = require('./environment')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
-
-module.exports = environment
+module.exports = environment.toWebpackConfig()
